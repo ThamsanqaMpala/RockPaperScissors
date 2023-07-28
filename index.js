1b3995cf0e3a39860594f5a224cd10a202cc4379
ThamsanqaMpala <thamsanqatmmpala@gmail.com>
2023-07-28 23:12:30 +0200

    Edit index.html and index.js

--- index.js
+++ index.js
@@ -14,63 +14,94 @@ function getComputerChoice(playerSelection){
     const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
     const randomNumber = getRandomNumber(1, 3);
 
+    let compInput = "";
     switch(randomNumber){
         case 1:
             console.log("Computer Picked Rock");
+            compInput = "ROCK";
             break;
         case 2:
             console.log("Computer Picked Paper");
+            compInput = "PAPER";
             break;
         case 3:
             console.log("Computer Picked Scissors");
+            compInput = "SCISSORS";
             break;
         default:
             console.log("Oops something went wrong")
     }
-    if(playerSelection == randomNumber ){
-        console.log("Tie");
-    }
+    return compInput;
 }
-function roundsPlayed(playerSelection, computerSelection){
+function playRounds(playerSelection, computerSelection){
     let roundNumuber = 1;
-        /*do {
-            console.log(`Round ${roundNumuber}`);
-            const computerChoice = computerSelection();
+    let playAgain = false;
+    let playerScore = 0;
+    let computerScore = 0;
 
-            let playAgain = confirm("Play Again?");
-            if(playAgain == true){
+        do {
+            console.log(`Round ${roundNumuber}`);
+            console.log(`Player Score = ${playerScore}`);
+            console.log(`Computer Score = ${computerScore}`);
+
+            let computerChoice = computerSelection();
+            let playerChoice = playerSelection();
+
+            if (computerChoice == playerChoice) {
+                console.log("Tie");
+            } else if ((computerChoice == "ROCK") && (playerChoice == "PAPER")) {
+                console.log(`${playerChoice} beats ${computerChoice}. ${playerChoice} wins`);
+                playerScore++;
+            } else if ((computerChoice == "PAPER") && (playerChoice == "ROCK")) {
+                console.log(`${computerChoice} beats ${playerChoice}. ${computerChoice} wins`);
+                computerScore++;
+            } else if ((computerChoice == "ROCK") && (playerChoice == "SCISSORS")) {
+                console.log(`${computerChoice} beats ${playerChoice}. ${computerChoice} wins`);
+                computerScore++;
+            } else if ((playerChoice == "ROCK") && (computerChoice == "SCISSORS")) {
+                console.log(`${playerChoice} beats ${computerChoice}. ${playerChoice} wins`);
+                playerScore++;
+            } else if ((computerChoice == "SCISSORS") && (playerChoice == "PAPER")) {
+                console.log(`${computerChoice} beats ${playerChoice}. ${computerChoice} wins`);
+                computerScore++;roundcompNumuber
+            } else if ((computerChoice == "PAPER") && (playerChoice == "SCISSORS")) {
+                console.log(`${playerChoice} beats ${computerChoice}. ${playerChoice} wins`);
+                playerScore++;
+            }
+
+
+            let anotherGame = prompt("Play again, YES(Y) or NO(N) ?");
+            anotherGame = anotherGame.toUpperCase();
+            if (anotherGame == "YES" || anotherGame == "Y") {
+                playAgain = true;
                 roundNumuber++;
             }else{
-                console.log("GAME OVER!!");
-                break;
+                process.exit(0);
             }
-            
-
-        } while (roundNumuber <= 5);*/
-
-    console.log(`Round ${roundNumuber}`);
-    console.log("Select your choice");
-    
-
+        } while (playAgain = true);
+    return;
 }
 
 
 function playerSelection(value){
 
-    switch(value){
-        case 1:
+    console.log("What`s your choice");
+    let playerInput = prompt("Rock, Paper or Scissors ? ");
+    playerInput = playerInput.toUpperCase();
+    switch(playerInput){
+        case "ROCK":
             console.log("Player Picked Rock");
             break;
-        case 2:
+        case "PAPER":
             console.log("Player Picked Paper");
             break;
-        case 3:
+        case "SCISSORS":
             console.log("Player Picked Scissors");
             break;
         default:
             console.log("Oops something went wrong")
     }
-    getComputerChoice(value);
+    return playerInput;
 }
 
 
